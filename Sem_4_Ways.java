c895fa3134ae67987498c8116bd9cdd131d4d2ef
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-12-22 02:07:10 +0300

    task solved

--- Sem_4_Ways.java
+++ Sem_4_Ways.java
@@ -1,31 +1,23 @@
-import java.util.Scanner;
-
 public class Sem_4_Ways {
-     
-      public static void main(String[] args) {
-        System.out.println(F(2, 22222));
-      }
-    
-      //
-      // N ->
-      // / F(N-1) , N mod 2 != 0
-      // F(N) = { F(N-1) + F(N/2), N mod 2 = 0
-      // \ 1, N = 2
-      //
-      //
-      static int F(int a, int b) {
-        //System.out.println(b);
-        // new Scanner(System.in).nextLine();
-        if (b == a)
-          return 1;
-        else if (b < a) {
-          return 0;
-        } else if (b % 2 != 0)
-          return F(a, b - 1);
-        else
-          return F(a, b - 1) + F(a, b / 2);
-      }
-}
-    
-    // N ->  
 
+    public static void main(String[] args) {
+        System.out.println(f(25000));
+    }
+
+    private static int f(int n) {
+        if (n < 2)
+            return 1;
+
+        int[] fs = new int[n];
+        fs[0] = fs[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            if (i % 2 == 0) {
+                fs[i] = fs[i / 2] + fs[i / 2 - 1];
+            } else {
+                fs[i] = fs[(i - 1) / 2] + fs[(i - 1) / 2 - 1];
+            }
+        }
+        return fs[n - 1];
+    }
+}
